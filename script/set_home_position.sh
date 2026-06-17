2316f30cdb4f15a86bba4aba3891e6f1c183c984
fincatomarta <fincatomarta16@gmail.com>
2026-06-17 12:45:58 +0000

    Added home scirpt

---
+++ script/set_home_position.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "set pos 0 -2.88" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 1 19.49" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 7 70.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 8 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 9 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 10 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 11 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 12 0.0" | yarp write ... /cer/right_arm/rpc:i
+ 
+echo "set pos 0 -2.88" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 1 19.49" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 7 70.0" | yarp write ... /cer/left_arm/rpc:i
+echo "set pos 8 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 9 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 10 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 11 0.0" | yarp write ... /cer/right_arm/rpc:i
+echo "set pos 12 0.0" | yarp write ... /cer/right_arm/rpc:i
+ 
+echo "set pos 0 0.012" | yarp write ... /cer/torso/rpc:i
+echo "set pos 3 0.0" | yarp write ... /cer/torso/rpc:i
+ 
+echo "set pos 0 0.0" | yarp write ... /cer/head/rpc:i
+echo "set pos 1 0.0" | yarp write ... /cer/head/rpc:i
---
+++ script/set_home_position_sim.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "set pos 0 -2.88" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 1 19.49" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 7 70.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 8 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 9 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 10 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 11 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 12 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+ 
+echo "set pos 0 -2.88" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 1 19.49" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 7 70.0" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 8 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 9 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 10 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 11 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 12 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+ 
+echo "set pos 0 0.012" | yarp write ... /r1mk3Sim/torso/rpc:i
+echo "set pos 3 0.0" | yarp write ... /r1mk3Sim/torso/rpc:i
+ 
+echo "set pos 0 0.0" | yarp write ... /r1mk3Sim/head/rpc:i
+echo "set pos 1 0.0" | yarp write ... /r1mk3Sim/head/rpc:i
